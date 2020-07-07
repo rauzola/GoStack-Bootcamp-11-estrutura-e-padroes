8e48bff4da410b46b2fef5691b3ab00af76529ca
rauzola <raul_sigoli@hotmail.com>
2020-07-07 11:36:19 -0300

    Debugando o NodeJS

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,7 +2,14 @@ import { Router } from "express";
 
 const routes = Router();
 
-routes.get("/", (request, response) => {
+routes.post("/users", (request, response) => {
+  const { name, email } = request.body;
+
+  const user = {
+    name,
+    email,
+  };
+
   response.json({ message: "Hello GoStack" });
 });
 

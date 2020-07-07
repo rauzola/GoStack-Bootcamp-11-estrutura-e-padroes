874d067d4b2612d6e561fce8e150b6096f9b53fc
rauzola <raul_sigoli@hotmail.com>
2020-07-07 10:48:45 -0300

    ESLint

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ import express from "express";
 const app = express();
 
 app.get("/", (request, response) => {
-  return response.json({ message: "Hello GoStack" });
+  response.json({ message: "Hello GoStack" });
 });
 
 const porta = 3333;
